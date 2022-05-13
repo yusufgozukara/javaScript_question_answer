73d34c70c0f7628169aec135956ce3ee521064cc
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2022-05-13 17:07:16 +0300

    Update reverse_sentence.js

--- reverse_sentence.js
+++ reverse_sentence.js
@@ -4,3 +4,48 @@ let sentence = "Bu yazıyı komple ters çevirmek istiyorum.";
 let sentmod = sentence.split("").reverse().join("").split(" ").reverse().join(" ");
 
 console.log(sentmod);
+
+
+
+//2.solution
+
+let a = [];
+let b = sentence.split(" ")
+
+for(let i = 0; i < b.length ; i++){
+    let c = b[i].split("").reverse().join("")
+    a = a + " " + c;  
+}
+console.log(a.trim());
+
+//3.solution
+
+let e = sentence.split(" ");
+// console.log(e);
+
+function reverse(word){
+    let d = [];
+    word.forEach(element => {
+        d.push(element.split("").reverse().join("")) 
+    }  
+    );
+    return d.join(" ");
+}
+console.log(reverse(e));
+
+//extra example same solution
+
+let sen = "tersine çevir gitsin";
+
+let x = sen.split(" ");
+
+function rev(word){
+    let y = [];
+    word.forEach(element => {
+        y.push(element.split("").reverse().join(""))
+        });
+    return y.join(" ");
+}
+console.log(rev(x));
+
+
