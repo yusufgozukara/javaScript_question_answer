9471238d40102da05b11cc75189551aef7c3ff3a
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2022-05-23 22:17:13 +0300

    Add files via upload

---
+++ triangle_area.js
@@ -0,0 +1,8 @@
+let taban = +prompt("taban değerini giriniz.");
+let yükseklik = +prompt("yükseklik değerini giriniz.");
+let alan;
+
+function alanhesap(){
+    return alan = (taban * yükseklik) / 2;
+}
+console.log(alanhesap());
