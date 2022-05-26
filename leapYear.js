98bdeb1d4feef478e92615392bcc1afb43522283
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2022-05-26 07:50:47 +0300

    Update leapYear.js

--- leapYear.js
+++ leapYear.js
@@ -1,6 +1,6 @@
 let year = +prompt("Please enter a year value.");
 
-if((year % 100 !== 0 || year % 4 == 0) && year % 400 == 0 ){
+if((year % 100 !== 0 && year % 4 == 0) || year % 400 == 0 ){
     alert(`${year} is a leap year`);
 } else{
     alert(`${year} is not a leap year.`);
